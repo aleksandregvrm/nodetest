31f3d2d6fda818a8d4ad7837e92d420cf315e223
aleksandregvrm <aleksandregvrm@gmail.com>
2024-06-07 12:29:57 +0400

    i also changed the arr length and numbers in it

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const something = 8;
 const somethingElse = 12345;
 
-const arr = [12,3,4,5,6,7,];
+const arr = [12,3,4,5,6,7,10,12,12,12,12,12,12,11];
 
 const newArr = arr.map((num)=>num+1);
 console.log(newArr);
